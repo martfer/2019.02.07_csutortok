44800b0b5134f4c3bb89a4b54eb804dd639dbda8
Martin Feri <ferenc.martin@dpc.hu>
2019-02-07 14:20:02 +0100

    added method.invoke()

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -6,10 +6,17 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class App {
+    private String name;
+
+    public App(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
 
-        App app = new App();
+        App first = new App("first");
+        App second = new App("second");
+
 
         Method[] methods = App.class.getDeclaredMethods();
         for (Method m : methods) {
@@ -17,7 +24,7 @@ public class App {
             if (m.getAnnotation(Test.class) != null) {
                 System.out.println(m);
 
-//                m.invoke(app);
+                m.invoke(first);
             }
         }
     }
@@ -25,32 +32,32 @@ public class App {
 
     @Test
     public void method1() {
-        System.out.println("running method1()");
+        System.out.println("running method1() in instance " + name);
     }
 
 
     public void method2() {
-        System.out.println("running method2()");
+        System.out.println("running method2() in instance " + name);
     }
 
 
     public void method3() {
-        System.out.println("running method3()");
+        System.out.println("running method3() in instance "  + name);
     }
 
     @Test
     public void method4() {
-        System.out.println("running method6()");
+        System.out.println("running method6() in instance " + name);
     }
 
 
     public void method5() {
-        System.out.println("running method6()");
+        System.out.println("running method6() in instance " + name);
     }
 
 
     public void method6() {
-        System.out.println("running method6()");
+        System.out.println("running method6() in instance " + name);
     }
 
 
