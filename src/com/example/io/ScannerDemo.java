cd59b2ec7c1b0c8fa7fd9b0880d8aea3c3d5228b
Martin Feri <ferenc.martin@dpc.hu>
2019-02-13 14:48:05 +0100

    added IO exercise, tokenise tax number

---
+++ src/com/example/io/ScannerDemo.java
@@ -0,0 +1,44 @@
+package com.example.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Path path = Path.of("/Users/martfer/Documents/Takarékinfo/delphi/2019.02.07_csutortok/adoszamok.txt");
+
+//        try (Scanner lineScanner = new Scanner(path), StandardCharsets.UTF_8)  {
+//            while(lineScanner.hasNextLine()) {
+//                String line = lineScanner.nextLine();
+//                System.out.println(tokenizeLine(line));
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(path.toFile(), StandardCharsets.UTF_8))) {
+            String line = null;
+            while( (line = br.readLine()) != null) {
+                System.out.println(tokenizeLine(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static String tokenizeLine(String line) {
+        String result = "";
+        Scanner scanner = new Scanner(line).useDelimiter("-");
+        while (scanner.hasNext()) {
+            result += scanner.next() + "\t|\t";
+        }
+        return result;
+    }
+}
