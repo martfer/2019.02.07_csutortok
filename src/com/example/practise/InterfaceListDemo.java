e66fac316beca746254ac6fbe5efb53a0b4d79de
Martin Feri <ferenc.martin@dpc.hu>
2019-02-14 13:46:44 +0100

    List interfaces demo and boxing unboxing code

---
+++ src/com/example/practise/InterfaceListDemo.java
@@ -0,0 +1,39 @@
+package com.example.practise;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class InterfaceListDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("All Implemented Interfaces:");
+        System.out.println(showInterfaces(JTextField.class));
+
+    }
+
+
+    public static String showInterfaces( Class c) {
+        Set<Class> s = new HashSet<>();
+
+        do {
+            for (Class iface : c.getInterfaces()) {
+                // add the interface to the Set
+                s.add(iface);
+                // and add all interfaces that it extends
+                s.addAll(Arrays.asList(iface.getInterfaces()));
+            }
+        } while ((c = c.getSuperclass()) != null);
+
+        StringBuilder sb = new StringBuilder();
+        for (Class iface : s) {
+                sb.append(iface.getSimpleName());
+                sb.append(", ");
+
+        }
+        // exclude the trailing ", "
+        return sb.substring(0, sb.length() - 2);
+    }
+}
